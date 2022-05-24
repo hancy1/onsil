34664697f26dfc7d8e76d6c63c7e261257d30f13
codud <codud@hcy.mshome.net>
2022-05-24 11:34:33 +0900

    방명록 부분 수정 중 - 채영

--- src/main/java/com/uni/spring/garden/model/dto/VisitorComment.java
+++ src/main/java/com/uni/spring/garden/model/dto/VisitorComment.java
@@ -1,5 +1,26 @@
 package com.uni.spring.garden.model.dto;
 
-public class VisitorComment {
+import java.sql.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class VisitorComment {//방명록댓글
+
+	private String commentNo; //댓글번호
+	private String boardNo; // 글번호
+	private String userNo; //회원번호
+	private String content; //내용
+ 	private Date enrollDate; //등록일
+	private String status; //상태	
+	
 }
--- src/main/java/com/uni/spring/garden/model/service/GardenServiceImpl.java
+++ src/main/java/com/uni/spring/garden/model/service/GardenServiceImpl.java
@@ -1,10 +1,13 @@
 package com.uni.spring.garden.model.service;
 
+import java.util.ArrayList;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.uni.spring.garden.model.dao.GardenDao;
+import com.uni.spring.garden.model.dto.VisitorBoard;
 
 
 
@@ -16,4 +19,14 @@ public class GardenServiceImpl implements GardenService{
 	
 	@Autowired
 	private GardenDao gardenDao;
+
+	@Override
+	public ArrayList<VisitorBoard> getBoardList(String userNo) {
+		
+		ArrayList<VisitorBoard> list = gardenDao.getBoardList(userNo, sqlSession);
+		
+		return list;
+	}
+
+	
 }
